afda9e25b418cfade96a6a325f57fe5fc1147d48
Aashi <165303579+Aktagh1234@users.noreply.github.com>
2025-05-19 12:44:32 +0000

    Copied into the first sub-directory

--- one/main.cpp
+++ one/main.cpp
@@ -7,12 +7,10 @@ int main() {
     std::cout<< "P3\n" << width << " " << height << "\n255\n";
     for (int y = height - 1; y >= 0; y--) {     // from top to bottom
         for (int x = 0; x < width; x++) {       // from left to right
-            float r = float(x) / float(width);  // normalize x to [0, 1]
-            float g = float(y) / float(height); // normalize y to [0, 1]
-            float b = 0.2f;                     // fixed blue value
-            int ir = int(255.99 * r);           // convert to int
-            int ig = int(255.99 * g);           // 255.99 is the max value for 8-bit color
-            int ib = int(255.99 * b);
+            vec3 col(float(x) / float(width),float(y)/float(height),0.2);  // create a color vector
+            int ir = int(255.99 * col[0]);           // convert to int
+            int ig = int(255.99 * col[1]);           // 255.99 is the max value for 8-bit color
+            int ib = int(255.99 * col[2]);           // 0.2 is the blue component
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
