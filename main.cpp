f0857cbf247caba1781d97b82e5dee6560fa1b60
Aashi <165303579+Aktagh1234@users.noreply.github.com>
2025-05-23 14:30:33 +0000

    Added many random multiple spheres

--- main.cpp
+++ main.cpp
@@ -28,6 +28,32 @@ vec3 color(const ray& r, hitable *world, int depth){
     }
 }
 
+hitable *random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];  // create an array of hitable pointers
+    list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5,0.5,0.5)));  
+    int i = 1;
+    for(int a=-11; a < 11; a++) {
+        for(int b=-11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+                }
+                else if (choose_mat < 0.95) {  // metal
+                    list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1+drand48()), 0.5*(1+drand48()), 0.5*(1+drand48())), 0.5*drand48()));
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4,1,0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));  
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7,0.6,0.5),0)); 
+    return new hitable_list(list, i);
+}
+
 int main() {
     //sphere=vec3(0,0,-1);  // set the center of the sphere
     //sphere_radius= 0.5;  // set the radius of the sphere
@@ -36,19 +62,20 @@ int main() {
     int ns=100;  // number of samples per pixel
     std::cout<< "P3\n" << width << " " << height << "\n255\n";
 
-    hitable *list[5];  // list of hitable objects
+    /*hitable *list[5];  // list of hitable objects
 
     list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));  // create a sphere object
     list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8,0.8,0.0)));  // create a large sphere object
     list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8,0.6,0.2),0.2));  // create a sphere object
     list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));  // create a sphere object
     list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));  // create a sphere object
-    hitable *world = new hitable_list(list, 5);  // create a hitable list with the spheres
+    hitable *world = new hitable_list(list, 5);  // create a hitable list with the spheres  */
     
-    vec3 lookfrom(3,3,2);  
-    vec3 lookat(0,0,-1);  
-    float dist_to_focus = (lookfrom - lookat).length();  
-    float aperture = 2.0;
+    hitable *world = random_scene(); 
+    vec3 lookfrom(13,2,3);  
+    vec3 lookat(0,0,0);  
+    float dist_to_focus = 10.0;  
+    float aperture = 0.1;
 
     camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(width)/float(height), aperture, dist_to_focus);  // create a camera object
     for (int y = height - 1; y >= 0; y--) {     // from top to bottom
